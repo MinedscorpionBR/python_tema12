d0004f1b91c99de63fce440d45b044d243c530d9
Minedscorpion <69402472+MinedscorpionBR@users.noreply.github.com>
2026-04-22 15:38:57 -0300

    Add interactive script to check for leftovers

---
+++ oqsobraprobeta.py
@@ -0,0 +1,11 @@
+sobra = ""
+
+while sobra != "nada":
+    sobra = input("o que sobra pro beta? ")
+    if sobra != "nada":
+        print("não, não sobra nada")
+        
+
+print("brutal")
+print("so resta o churrascamento")
+print("moggado pelo chat")
